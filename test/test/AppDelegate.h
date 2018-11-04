93eb534d34ef6b48c7bcb64a4d25f6795f10d9b0
llbt <705882260@qq.com>
2018-11-04 15:50:55 +0800

    add

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by llbt on 2018/11/4.
+//  Copyright © 2018年 shuaiChuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by llbt on 2018/11/4.
+//  Copyright © 2018年 shuaiChuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
